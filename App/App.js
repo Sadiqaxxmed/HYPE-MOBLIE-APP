dfa1a68a1a316eda6aa539a722751b77eb845345
sadiqaxxmed <sadiqaxxmed@gmail.com>
2024-03-04 22:12:13 -0800

    First commit

---
+++ App/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Test from './src/test';
+
+const App = () => {
+  return (
+
+    <View style={styles.container}>
+      {/* <Text style={styles.text}>Hello, React Native!</Text> */}
+      <Text>
+      <Test />
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
